bafebcbdc82d184a2e024cc37ba1aeecfe828a15
barrysweeney <barrysweeney172@live.co.uk>
2020-02-01 15:32:19 +0000

    change names and ordering of functions in file

--- script.js
+++ script.js
@@ -1,50 +1,47 @@
 const container = document.getElementById('container');
+const resetButton = document.getElementById('reset');
+const squares = document.getElementsByClassName('item')
+let squaresPerSide = 4;
 
-        let squaresPerSide = 4;
+// setup the initial grid
+createDivs(squaresPerSide);
+makeDrawable();
 
-        function createDivs(numDivs) {
-            for (let i = 1; i <= squaresPerSide ** 2; i++) {
-                const div = document.createElement('div')
-                div.className = "item"
-                div.id = `item${i}`;
-                container.appendChild(div);
-            }
-        }
-        createDivs(squaresPerSide);
+resetButton.addEventListener('click', function (event) {
+    for (i = 0; i < squares.length; i++) {
+        squares[i].style.cssText = "background-color: white;";
+    }
+    squaresPerSide = prompt("How many squares per side would you like your new grid?");
+    let newGridSize = repeatString("auto ", squaresPerSide);
+    container.style.cssText = `grid-template-columns: ${newGridSize};`;
+    container.innerHTML = '';
+    createDivs(squaresPerSide);
+    makeDrawable();
+})
 
-        const boxes = document.getElementsByClassName('item')
+function createDivs(numDivs) {
+    for (let i = 1; i <= squaresPerSide ** 2; i++) {
+        const div = document.createElement('div')
+        div.className = "item"
+        div.id = `item${i}`;
+        container.appendChild(div);
+    }
+}
 
-        function makeDrawable() {
-            for (i = 0; i < boxes.length; i++) {
-                boxes[i].addEventListener('mouseenter', function (event) {
-                    event.target.style.cssText = "background-color: #92A8D1;"
-                })
-            }
-        }
-        makeDrawable();
-
-        const btn = document.getElementById('reset');
-
-
-        btn.addEventListener('click', function (event) {
-            for (i = 0; i < boxes.length; i++) {
-                boxes[i].style.cssText = "background-color: white;";
-            }
-            squaresPerSide = prompt("How many squares per side would you like your new grid?");
-            let newGridSize = repeatString("auto ", squaresPerSide);
-            container.style.cssText = `grid-template-columns: ${newGridSize};`;
-            container.innerHTML = '';
-            createDivs(squaresPerSide);
-            makeDrawable();
+function makeDrawable() {
+    for (i = 0; i < squares.length; i++) {
+        squares[i].addEventListener('mouseenter', function (event) {
+            event.target.style.cssText = "background-color: #92A8D1;"
         })
+    }
+}
 
-
-        const repeatString = function (myString, times) {
-            let stringToReturn = "";
-            if (times < 0) { return "ERROR" } else {
-                for (let i = 1; i <= times; i++) {
-                    stringToReturn += myString;
-                }
-                return stringToReturn;
-            }
+const repeatString = function (myString, times) {
+    let stringToReturn = "";
+    if (times < 0) { return "ERROR" } else {
+        for (let i = 1; i <= times; i++) {
+            stringToReturn += myString;
         }
+        return stringToReturn;
+    }
+}
